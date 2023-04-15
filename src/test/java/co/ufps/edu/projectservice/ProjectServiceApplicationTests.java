881487407ce9b5e34ad3e1a0bfbee49a3f76b1e2
Yeison07 <yeisona07@hotmail.com>
2023-04-14 21:18:50 -0500

    project service initial commit

---
+++ src/test/java/co/ufps/edu/projectservice/ProjectServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ufps.edu.projectservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
